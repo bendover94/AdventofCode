d74fb9dc8e9ed15a7fcf3ba4c12babfecbbf3188
bendover94 <l.schmirl@pernauer.com>
2022-12-06 21:59:20 +0100

    first commit

---
+++ day_1/calorieCounting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+// ofstream     Creates and writes to files
+// ifstream     Reads from files
+// fstream	    A combination of ofstream and ifstream: creates, reads, and writes to files
+
+
+
+
+int main()
+{   
+
+
+
+    return 0;
+}
